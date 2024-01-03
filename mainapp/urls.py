4795ef8b098681f589d3b93e638c705bbf2d59a5
Maksim <maksim.bobrikov2015@yandex.ru>
2024-01-04 00:03:33 +0300

    first apperance pages

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path("", views.main, name="main"),
     path("home", views.main, name="home"),
     path("home/", views.main, name="home"),
-    path("words_list", views.main, name="words_list"),
-    path("words_list/", views.main, name="words_list"),
-    path("add_word", views.main, name="add_word"),
-    path("add_word/", views.main, name="add_word"),
+    path("words_list", views.words_list, name="words_list"),
+    path("words_list/", views.words_list, name="words_list"),
+    path("add_word", views.add_word, name="add_word"),
+    path("add_word/", views.add_word, name="add_word"),
+    path("1", views.index, name="index"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -5,3 +5,22 @@ from django.http import HttpResponse
 def main(request):
     print(request)
     return render(request, "home.html")
+
+def words_list(request):
+    print(request)
+    return render(request, "words_list.html")
+
+def add_word(request):
+    print(request)
+    if request.method == "GET":
+        return render(request, "add_word.html")
+
+    else:
+        print(request.POST)
+        #with open("dictionary.txt", "a", encoding="utf-8") as file:
+        #file.write(word1 + "-" + word2 + "\n")
+
+
+def index(request):
+    print(request)
+    return render(request, "index.html")
