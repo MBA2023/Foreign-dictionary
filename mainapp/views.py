2f2596ea6f26ad87fac9d9f4998f4d70198ba72e
Maksim <maksim.bobrikov2015@yandex.ru>
2024-01-05 01:20:15 +0300

    work on adding words...

--- mainapp/views.py
+++ mainapp/views.py
@@ -16,9 +16,10 @@ def add_word(request):
         return render(request, "add_word.html")
 
     else:
-        print(request.POST)
-        #with open("dictionary.txt", "a", encoding="utf-8") as file:
-        #file.write(word1 + "-" + word2 + "\n")
+        print("первый принт", request)
+        print("2 принт", request.POST)
+        with open("dictionary.txt", "a", encoding="utf-8") as file:
+            file.write(request)
 
 
 def index(request):
