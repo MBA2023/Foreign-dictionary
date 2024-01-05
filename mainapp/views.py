9215a42be0f299e3cde2218358bfd2484e4dda80
Maksim <maksim.bobrikov2015@yandex.ru>
2024-01-06 01:16:06 +0300

    Page of adding words is completed

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
@@ -19,7 +19,8 @@ def add_word(request):
         print("первый принт", request)
         print("2 принт", request.POST)
         with open("dictionary.txt", "a", encoding="utf-8") as file:
-            file.write(request)
+            file.writelines(f"New word: {request.POST['new_word']}, Translation: {request.POST['translation']}, Language: {request.POST['language']} \n")
+        return redirect(add_word)
 
 
 def index(request):
